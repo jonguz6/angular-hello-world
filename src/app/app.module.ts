ef92a58b34c47ba88e3e51a457c6f1affabb53a3
Jonatan <jonguz6@wp.pl>
2021-03-31 14:16:02 +0200

    Create 'Course component'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CourseComponent } from './course/course.component';
+import {CourseService} from './course/course.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [CourseService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/course/course.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import {CourseService} from './course.service';
+
+@Component({
+  selector: 'app-course',
+  templateUrl: './course.component.html',
+  styleUrls: ['./course.component.css']
+})
+export class CourseComponent implements OnInit {
+  title = 'List of courses';
+  courses;
+
+  constructor(service: CourseService) {
+    this.courses = service.getCourses();
+  }
+
+  ngOnInit(): void {
+  }
+
+
+}
